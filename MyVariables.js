cfb8b30eeaf5b6d0f96b93facb9cc3173758f21b
Christine <exp16240@adobe.com>
2022-12-01 09:10:39 -0800

    Update MyVariables.js
    updated variables file

---
+++ MyVariables.js
@@ -0,0 +1,10 @@
+var s_account = "adobecx1czak";
+var s = s_gi(s_account);
+
+s.trackingServer = "training.data.adobedc.net";
+
+s.pageName = "Czak home page";
+s.evar1 = "Example value";
+s.prop1 = "Prop";
+
+s.t();
